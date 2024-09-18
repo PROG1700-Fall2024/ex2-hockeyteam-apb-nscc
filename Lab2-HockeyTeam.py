0e08b824763ece4da8c8020c97f9899b1790947c
apb-nscc <W0487099@campus.nscc.ca>
2024-09-18 17:24:41 -0300

    Completed and functioning properly

--- Lab2-HockeyTeam.py
+++ Lab2-HockeyTeam.py
@@ -10,10 +10,18 @@
 #Purpose/Concepts: Input and output to screen, string concatentation, string formatting, datatype casting, 
 # simple math operations
 
+#team = ["Halifax Harpooners"]
+
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-    pass
+    print("Welcome to the Hockey Team Application!")
+    teamName = input("Just enter the name of the team you'd like to see to get started: ")
+    wins = input("How many wins do the {0} have? ".format(teamName))
+    losses = input("How many losses do the {0} have? ".format(teamName))
+    totalGames = int(wins )+ int(losses) #<--Interesting note, I had to debug to get this right. I initially had int(wins + losses) but that concatenated two strings and then get the int of that which is not what I wanted 
+    print("The {0} have {1} win(s) and {2} loss(es), with a win percentage of ".format(teamName,wins,losses) + str(round(int(wins) / totalGames,4)))
+
 
 
 
